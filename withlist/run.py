f7dc5dfbaeebcf275b60b0b416db57d8158fcf4d
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-08-06 01:49:17 +0900

    withlist/run.py: New: Run a command with locking list

---
+++ withlist/run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+##
+## Mailman withlist: Run command with locking list
+## Copyright (c) 2013 SATOH Fumiyasu @ OSS Technology Corp., Japan
+##
+## License: GNU General Public License version 2 or later
+##
+
+"""Usage: withlist -l -r run COMMAND [ARGUMENT ...]
+"""
+
+import sys
+import os
+
+def run(mlist, *args):
+    if not mlist.Locked():
+	print >>sys.stderr, 'List is not locked'
+	sys.exit(1)
+
+    pid = os.fork()
+    if pid == 0:
+	os.execvp(args[0], args)
+
+    pid_exited, status = os.waitpid(pid, 0)
+    sys.exit(status >> 8)
+
