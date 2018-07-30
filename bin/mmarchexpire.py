abad8ab3c9f01ea2317981b8c1bc92f2b33f661e
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2018-07-30 15:49:03 +0900

    mmarchexpire: Support expiring attachments for monthly archives

--- bin/mmarchexpire.py
+++ bin/mmarchexpire.py
@@ -32,6 +32,8 @@ import os
 import errno
 import sys
 import getopt
+import re
+import calendar
 import shutil
 import cPickle as pickle
 
@@ -47,6 +49,23 @@ from Mailman import i18n
 C_ = i18n.C_
 
 i18n.set_language(mm_cfg.DEFAULT_SERVER_LANGUAGE)
+
+month_by_name = {
+  'January': 1,
+  'February': 2,
+  'March': 3,
+  'April': 4,
+  'May': 5,
+  'June': 6,
+  'July': 7,
+  'August': 8,
+  'September': 9,
+  'October': 10,
+  'November': 11,
+  'December': 12,
+}
+date_is_daily = re.compile(r'^\d{8}$')
+date_is_monthly = re.compile(r'^(?P<year>\d{4})-(?P<month_name>' + '|'.join(month_by_name) + r')$')
 
 
 def usage():
@@ -90,7 +109,7 @@ def main():
     listname = args[0].lower().strip()
     num = int(args[1])
     if num < 1:
-        pdie(1, 'FIXME')
+        pdie(1, 'Number (>0) required')
 
     mlist = None
     try:
@@ -102,12 +121,13 @@ def main():
         index = os.path.join(mlist.archive_dir(), 'pipermail.pck')
         with open(index, 'r') as f:
             d = pickle.load(f)
- 
+
         if not (len(d['archives']) > num):
             if verbose_p:
                 print("No expirations", file=sys.stderr)
             return
 
+        a_dir = os.path.join(mlist.archive_dir(), 'attachments')
         while len(d['archives']) > num:
             date = d['archives'].pop()
             date_dir = os.path.join(mlist.archive_dir(), date)
@@ -115,10 +135,17 @@ def main():
 
             paths = [date_dir, date_dir + '.txt', date_dir + '.txt.gz']
             paths.extend([date_db + x for x in ('article', 'author', 'date', 'subject', 'thread')])
-            if mlist.archive_volume_frequency == 4: ## Daily
-                paths.append(os.path.join(mlist.archive_dir(), 'attachments', date))
+            if date_is_daily.match(date):
+                paths.append(os.path.join(a_dir, date))
+            elif date_is_monthly.match(date):
+                m = date_is_monthly.match(date)
+                year = int(m.group('year'))
+                month = month_by_name[m.group('month_name')]
+                mdays = calendar.monthrange(year, month)[1]
+                for mday in range(1, mdays+1):
+                    paths.append(os.path.join(a_dir, '%d%02d%02d' % (year, month, mday)))
             else:
-                pdie(3, 'FIXME: mlist.archive_volume_frequency must be 4 (Daily)')
+                print('Unsupported date format: %s' % date, file=sys.stderr)
 
             for path in paths:
                 if verbose_p:
