45a49fc0d457eb9749e53fe2e20b7a7edcfe6685
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2022-12-15 16:54:34 +0900

    fml-mkcpio-lists: Use my getopts implementation, support non GNU find
    `find -files0-from ...` requires GNU findutils 4.9.0

--- bin/fml-mkcpio-lists.bash
+++ bin/fml-mkcpio-lists.bash
@@ -9,16 +9,63 @@
 set -u
 set -o pipefail || exit $?		## bash 3.0+
 
-without_htdocs_p="set"
-if [[ ${1-} == @(--with-htdocs) ]]; then
-  shift
-  without_htdocs_p=""
-fi
-without_spool_p=""
-if [[ ${1-} == @(-S|--without-spool) ]]; then
-  shift
-  without_spool_p="set"
-fi
+## ======================================================================
+
+getopts_want_arg()
+{
+  if [[ $# -lt 2 ]]; then
+    pdie "Option requires an argument: $1"
+  fi
+  if [[ -n ${3:+set} ]]; then
+    if [[ $2 =~ $3 ]]; then
+      : OK
+    else
+      pdie "Invalid value for option: $1: $2"
+    fi
+  fi
+  if [[ -n ${4:+set} ]]; then
+    if [[ $2 =~ $4 ]]; then
+      pdie "Invalid value for option: $1: $2"
+    fi
+  fi
+}
+
+## ----------------------------------------------------------------------
+
+export without_htdocs_p="set"
+export without_spool_p=""
+
+while [[ $# -gt 0 ]]; do
+  opt="$1"; shift
+
+  if [[ -z "${opt##-[!-]?*}" ]]; then
+    set -- "-${opt#??}" ${1+"$@"}
+    opt="${opt%${1#-}}"
+  fi
+  if [[ -z "${opt##--*=*}" ]]; then
+    set -- "${opt#--*=}" ${1+"$@"}
+    opt="${opt%%=*}"
+  fi
+
+  case "$opt" in
+  --with-htdocs)
+    without_htdocs_p=""
+    ;;
+  -S|--without-spool)
+    without_spool_p="set"
+    ;;
+  --)
+    break
+    ;;
+  -*)
+    pdie "Invalid option: $opt"
+    ;;
+  *)
+    set -- "$opt" ${1+"$@"}
+    break
+    ;;
+  esac
+done
 
 if [[ $# != 2 ]]; then
   prog_name="${0##*/}"
@@ -36,45 +83,50 @@ EOF
   exit 1
 fi
 
+## ======================================================================
+
 fml_lists_dir="$1"; shift
 cpio_gz="$1"; shift
 
 (
   cd "$fml_lists_dir" || exit $?
-  # shellcheck disable=SC2185 # finds don't have a default path. Specify '.' explicitly
+  # shellcheck disable=SC2016 # Expressions don't expand in single quotes, use double quotes for that
   find . \
     -mindepth 1 \
     -maxdepth 1 \
     -type d \
     ! -name '@*' \
+    ! -name '-*' \
     -print0 \
-  |find \
-    -files0-from - \
-    -mindepth 1 \
-    -maxdepth 3 \
-    -type d \( \
-      -name var -o \
-      ${without_htdocs_p:+-name htdocs -o} \
-      ${without_spool_p:+-name spool -o} \
-      -false \
-    \) \
-    -prune \
-    -o \
-    ! -name '.crc' \
-    ! -name log \
-    ! -name summary \
-    ! -name 'actives.[0-9]' \
-    ! -name 'actives.[0-9][0-9]' \
-    ! -name 'members.[0-9]' \
-    ! -name 'members.[0-9][0-9]' \
-    ! -name 'fmlwrapper.[ch]' \
-    ! -name '*.bak' \
-    ! -name '*.db' \
-    ! -name '*.dir' \
-    ! -name '*.pag' \
-    ! -name '*.old' \
-    -type f \
-    -print0 \
+  |xargs -0 sh -"${-//[!x]}c" '
+    find \
+      "$@" \
+      -mindepth 1 \
+      -maxdepth 3 \
+      -type d \( \
+        -name var -o \
+        ${without_htdocs_p:+-name htdocs -o} \
+        ${without_spool_p:+-name spool -o} \
+        -false \
+      \) \
+      -prune \
+      -o \
+      ! -name ".crc" \
+      ! -name log \
+      ! -name summary \
+      ! -name "actives.[0-9]" \
+      ! -name "actives.[0-9][0-9]" \
+      ! -name "members.[0-9]" \
+      ! -name "members.[0-9][0-9]" \
+      ! -name "fmlwrapper.[ch]" \
+      ! -name "*.bak" \
+      ! -name "*.db" \
+      ! -name "*.dir" \
+      ! -name "*.pag" \
+      ! -name "*.old" \
+      -type f \
+      -print0 \
+    ' sh \
   |cpio  \
     --quiet \
     -0 \
